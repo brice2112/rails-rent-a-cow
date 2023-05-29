5e76e141e939f15e65c15356410462ef26ac353e
Helene Coulombeix <helenecoulombeix@yahoo.fr>
2023-05-29 16:58:07 +0200

    creation routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  resources :cows, only: [index, show, create, delete] do
+    resources :bookings, only: [index, show, create, edit, update]
+  end
+end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-end
